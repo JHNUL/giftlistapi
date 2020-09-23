ab0a737c15ca6b81b2648976ddc8c4077cfe21ae
Juhanir <juhani.riisio@gmail.com>
2020-09-24 00:31:16 +0300

    create mock repository classes and initial unit test

--- src/repositories/ItemRepository.ts
+++ src/repositories/ItemRepository.ts
@@ -6,19 +6,19 @@ import { IItemModel, ItemModel } from '../models/ItemModel';
 @Service()
 export class ItemRepository {
 
-  async findById(id: string): Promise<IItemModel | null> {
+  public async findById(id: string): Promise<IItemModel | null> {
     const objectId = mongoose.Types.ObjectId(id);
     return await ItemModel.findById(objectId);
   }
 
-  async findAll(reserved?: boolean): Promise<IItemModel[]> {
+  public async findAll(reserved?: boolean): Promise<IItemModel[]> {
     const findConditions = typeof reserved === 'boolean'
       ? { reserved }
       : {};
     return await ItemModel.find(findConditions);
   }
 
-  async insert(input: ItemInput): Promise<IItemModel> {
+  public async insert(input: ItemInput): Promise<IItemModel> {
     const newItem = new ItemModel(input.itemInput);
     return await newItem.save();
   }

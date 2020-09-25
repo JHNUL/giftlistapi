5e07d561b4b8c7b3f86a2531e521900df15231d4
Juhanir <juhani.riisio@gmail.com>
2020-09-26 01:35:08 +0300

    pass config object to apollo server listen

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const init = async (): Promise<void> => {
     await connectToDb();
     console.log('connected to mongodb..');
     const server = new ApolloServer(serverConfig);
-    const { url } = await server.listen();
+    const { url } = await server.listen({ port: process.env.PORT || 4000 });
     console.log(`Playground available at ${url}`);
   } catch (error) {
     console.error(error);
